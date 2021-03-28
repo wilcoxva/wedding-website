8a2867c6ffd4767e4ca8d25f969438b9d8e57e58
Virginia Wilcox <wilcoxva@gmail.com>
2021-03-28 09:03:45 -0400

    Added timeleft

--- wedding-website/src/App.js
+++ wedding-website/src/App.js
@@ -7,6 +7,19 @@ function App() {
   const calculateTimeLeft = () => {
 
     const difference = +new Date(`05/07/2022`) - +new Date();
+
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60)
+      };
+    }
+
+    return timeLeft;
     
   }
 
